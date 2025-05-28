114458fc3caee784a0e3af7461464dae7963ccd0
anyway3 <113296263+anyway3@users.noreply.github.com>
2025-05-28 16:03:19 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	int a;
+	int b;
+	int c;
+
+	std::cin >> a >> b >> c;
+	std::cout << (a + b) % c << std::endl;
+	std::cout << ((a + c)+(b%c))%c << std::endl;
+	std::cout << (a * b) % c << std::endl;
+	std::cout << ((a + c) * (b % c)) % c << std::endl;
+
+}
