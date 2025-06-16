d01810472632c1484554cf4f0508a6dbf06461dd
anyway3 <113296263+anyway3@users.noreply.github.com>
2025-06-16 20:22:31 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.60 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    for (int i = 1; i < food.size(); i++) {
+        for (int j = 0; j < food[i]/2; j++) {
+                answer = answer + to_string(i);
+
+
+            
+        }
+
+    }
+    answer = answer + "0";
+
+    for (int i = answer.size()-2; i >= 0; i--) {
+        answer = answer + answer[i];
+
+    }
+
+    return answer;
+}
