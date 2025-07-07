2a866a907fe8da793ccd2c6bb63acfdb6ca385ea
anyway3 <113296263+anyway3@users.noreply.github.com>
2025-07-07 10:14:03 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -1,14 +1,13 @@
 #include <string>
 #include <vector>
-
+#include<iostream>
+#include <algorithm>
 using namespace std;
-string a ;
+string a;
 string solution(vector<string> seoul) {
-    for(int i =0;i<seoul.size();i++){
-        if(seoul[i]=="Kim"){
-            a=to_string(i);
-        }
-    }
-    string answer = "김서방은 "+a+"에 있다";
+    auto it = find(seoul.begin(), seoul.end(), "Kim");
+    int a = static_cast<int>(it - seoul.begin());
+
+    string answer = "김서방은 " + to_string(a) + "에 있다";
     return answer;
 }
