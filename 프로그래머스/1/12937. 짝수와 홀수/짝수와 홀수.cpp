eab3627cccb679da2fd291d590f2460ccd14449d
anyway3 <113296263+anyway3@users.noreply.github.com>
2025-07-08 15:35:27 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 string solution(int num) {
     string answer = "";
-    if(num%2==0){
-        answer="Even";
+    if (num % 2 == 0)
+    {
+        answer = "Even";
     }
-    else{
-        answer="Odd";
+    else
+    {
+        answer = "Odd";
     }
     return answer;
 }
