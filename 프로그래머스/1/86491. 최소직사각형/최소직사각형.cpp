8522c6c542c64de1dc20d286c28a7e4b423ea5db
anyway3 <113296263+anyway3@users.noreply.github.com>
2025-08-28 10:10:25 +0900

    [level 1] Title: 최소직사각형, Time: 1.12 ms, Memory: 5.03 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,29 +1,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
-
 
 int solution(vector<vector<int>> sizes) {
     int answer = 0;
-	int x = 0;
-	int y = 0;
-    for (int i = 0; i < sizes.size(); i++) {
-        sort(sizes[i].begin(), sizes[i].end());
+    int xmax=0;
+    int ymax=0;
+    for (auto& row : sizes) {
+        sort(row.begin(), row.end());
     }
-
-    for (int i = 0; i < sizes.size();i++) {
-        for (int ii = 0; ii < sizes[i].size(); ii++) {
-            if (ii == 0 && x < sizes[i][ii]) {
-				x = sizes[i][ii];
-
-            }
-            if (ii == 1 && y < sizes[i][ii]) {
-                y = sizes[i][ii];
-            }
+    for (int i = 0; i < sizes.size(); i++)
+    {
+        if (sizes[i][0] > xmax)
+        {
+            xmax = sizes[i][0];
+        }
+        if (sizes[i][1] > ymax)
+        {
+            ymax = sizes[i][1];
         }
     }
-	answer = x * y;
-        return answer;
+    answer = xmax * ymax;
+
+    return answer;
 }
