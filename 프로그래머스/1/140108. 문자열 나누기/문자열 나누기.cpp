ae6313fe9463f2b0ee5830b557e29d19b24e72ab
anyway3 <113296263+anyway3@users.noreply.github.com>
2025-09-25 10:17:59 +0900

    [level 1] Title: 문자열 나누기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -4,35 +4,40 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = 0;
-    char first = s[0];
-    int nop = 0;
+    char first = s[0] ;
     int yes = 0;
+    int nop = 0;
+    int answer = 0;
     for (int i = 0; i < s.size(); i++)
     {
         if (s[i] == first)
         {
             yes++;
         }
+
         else
         {
             nop++;
         }
+
+
         if (nop == yes)
         {
             answer++;
 
-            if (i + 1 < s.size()) {
+            if (i + 1 < s.size())
+            {
                 first = s[i + 1];
             }
             yes = 0;
             nop = 0;
         }
+
     }
+
     if (yes != 0 || nop != 0)
     {
         answer++;
     }
-
     return answer;
 }
